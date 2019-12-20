0d9ee7c9132dbd77810d93284702379e8d44876c
snekalatha23 <57349438+snekalatha23@users.noreply.github.com>
2019-12-20 17:24:53 +0530

    Create recur.cpp

---
+++ recur.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int factorial(int);
+int main() 
+{
+    int n;
+    cout<<"Enter a number to find factorial: ";
+    cin >> n;
+    cout << "Factorial of " << n <<" = " << factorial(n);
+    return 0;
+}
+int factorial(int n) 
+{
+    if (n > 1) 
+    {
+        return n*factorial(n-1);
+    }
+    else 
+    {
+        return 1;
+    }
+}
